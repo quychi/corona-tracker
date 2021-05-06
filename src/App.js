d908509fcca5f893d301831c5e4a40558b2772da
quychi <nguyenquychi96@gmail.com>
2021-05-06 22:28:40 +0700

    import in one line

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+// import Cards from './components/Cards/Cards';
+// import Chart from './components/Chart/Chart';
+// import CountryPicker from './components/CountryPicker/CountryPicker';
+
+// good import make code more readable
+import { Cards, Chart, CountryPicker } from './components';//and create index.js in folder components
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Cards />
+      <Chart />
+      <CountryPicker />
     </div>
   );
 }
